3fabd4296049100447bd0ceabbbaebc4b3349a6c
Kooda <kooda@upyum.com>
2016-06-16 16:25:23 +0200

    Begin an example attempt of frp+glfw
    Warning: broken commit, does not work yet!

--- frp-example.scm
+++ frp-example.scm
@@ -1,27 +1,101 @@
+(use glfw3 gl nonblocking-swap-buffers matchable)
+
 (load "frp")
 
+(define new-frame
+  (make-primitive-signal
+   (lambda (self children input)
+     (for-each
+      (lambda (c)
+        (gochan-send
+         c
+         (if (equal? (gochan-receive input) 'new-frame)
+             '(change new-frame)
+             '(no-change new-frame))))
+      children))))
+
+(define cursor-position
+  (make-primitive-signal
+   (let ((state '(cursor-position 0 0)))
+     (lambda (self children input)
+       (let ((msg (gochan-receive input)))
+         (for-each
+          (lambda (c)
+            (gochan-send
+             c
+             (match msg
+               (('change ('cursor-position x y))
+                (set! state (list x y))
+                (list 'change state))
+               (else (list 'no-change state)))))
+          children))))))
+
+;; (define clock (signal-map get-time new-frame))
+;; or something like that
 (define clock
-  (make-signal
-   (lambda (self children)
-     (thread-sleep! 1)
-     (for-each (lambda (c) (gochan-send c 2)) children))))
-
-(define counter
   (make-registered-signal
-   (list clock)
-   (let ((count 1))
-     (lambda (self children clock)
-       (for-each (lambda (c) (gochan-send c count)) children)
-       (set! count (* count (gochan-receive clock)))))))
+   (list new-frame)
+   (let ((state 0))
+     (lambda (self children new-frame)
+       (let ((msg (gochan-receive new-frame))
+             (now (get-time)))
+         (for-each
+          (lambda (c)
+            (gochan-send
+             c
+             (match msg
+               (('change _)
+                (set! state now)
+                (list 'change state))
+               (else
+                (list 'no-change state)))))
+          children))))))
 
-(define printer
+(define scene
   (make-registered-signal
-   (list counter clock)
-   (lambda (self children counter clock)
-     (printf "~A: ~A\n" (gochan-receive counter) (gochan-receive clock)))))
+   (list clock cursor-position)
+   (let ((state void))
+     (lambda (self children clock cursor-position)
+       (let ((now (gochan-receive clock))
+             (pos (gochan-receive cursor-position)))
+         (if (and (eq? (car now) 'no-change)
+                  (eq? (car pos) 'no-change))
+             (for-each (lambda (c) (gochan-send c (list 'no-change state))) children)
+             (begin
+               (set! state
+                 ;; represent scene as a thunk for now
+                 (lambda ()
+                   (print (list cursor-position: (first (cadr pos)) (second (cadr pos))))
+                   (gl:Clear gl:COLOR_BUFFER_BIT)
+                   (gl:LoadIdentity)
+                   (gl:Rotatef (* 100 (cadr now)) 0 0 1)
+                   (gl:Begin gl:QUADS)
+                   (gl:Vertex2f -0.2 0.2)
+                   (gl:Vertex2f 0.2 0.2)
+                   (gl:Vertex2f 0.2 -0.2)
+                   (gl:Vertex2f -0.2 -0.2)
+                   (gl:End)))
+               (for-each
+                (lambda (c) (gochan-send c (list 'change state)))
+                children))))))))
+
+(define scene-receiver (gochan))
+(emitters-set! scene (list scene-receiver))
 
 
-(use trace)
-(trace start-signal-graph!)
+(cursor-position-callback
+ (lambda (window x y)
+   (broadcast-message! (list 'cursor-position x y))))
 
-(start-signal-graph! printer)
+(with-window (600 400 "GLFW3 Test" resizable: #f)
+  (start-signal-graph! scene)
+  (let loop ()
+    ;; (nonblocking-swap-buffers)
+    ;; (gc #f)
+    ;; (wait-vblank)
+    (swap-buffers (window))
+    (poll-events)
+    (broadcast-message! 'new-frame)
+    ((cadr (gochan-receive scene-receiver)))
+    (unless (window-should-close (window))
+      (loop))))
