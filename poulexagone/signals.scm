862af3d2f3e66511565c1c545dcba8f5c62f369f
Kooda <kooda@upyum.com>
2016-07-11 09:50:49 +0200

    Poulexagone: use key names instead of codes

--- poulexagone/signals.scm
+++ poulexagone/signals.scm
@@ -15,15 +15,15 @@
 (define movement-keys
   (frp:map
    (lambda (k)
-     (let ((key (cadr k))
+     (let ((key (car k))
            (action (caddr k)))
-       (make-movement-key (case key ((38) 'left) ((40) 'right))
-                          (case action ((0) #f) ((1) #t)))))
+       (make-movement-key (select key ((+key-a+) 'left) ((+key-d+) 'right))
+                          (select action ((+release+) #f) ((+press+) #t)))))
    (frp:filter (lambda (k)
                  (and (pair? k)
-                      (not (= (caddr k) 2))
-                      (or (= (cadr k) 38) (= (cadr k) 40))))
-               '(0 38 0 0) key)))
+                      (not (= (caddr k) +repeat+))
+                      (member (car k) (list +key-a+ +key-d+))))
+               `(,+key-a+ 0 ,+release+ 0) key)))
 
 
 ;; Signal notifying about spacebar presses
@@ -34,7 +34,8 @@
   (frp:map
    (lambda (_) (make-spacebar-pressed))
    (frp:filter
-    (cut equal? <> '(32 65 1 0))
+    (lambda (k) (and (eq? (car k) +key-space+)
+                     (eq? (caddr k) +press+)))
     #f
     key)))
 
@@ -44,6 +45,7 @@
   (frp:map
    (lambda (_) (make-escape-pressed))
    (frp:filter
-    (cut equal? <> '(256 9 1 0))
+    (lambda (k) (and (eq? (car k) +key-escape+)
+                     (eq? (caddr k) +press+)))
     #f
     key)))
